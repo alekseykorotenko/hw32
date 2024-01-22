0d2ecb4a610d75a05c9184288ba467cb34b4a74e
alekseykorotenko <alekor86@gmail.com>
2024-01-22 16:26:14 +0200

    add all styles, jquery, animations, fonts etc. Final commit before pending

--- js/animation.js
+++ js/animation.js
@@ -12,4 +12,33 @@ $(function () {
       transition: '3s all'
     });
   });
+
+  $('div.elem').hover(
+    function () {
+      $(this).css(
+        {
+          transform: 'rotate(360deg)',
+          backgroundColor: 'greenyellow',
+          transition: '3s all'
+        },
+        $('div.elem__square').css({
+          backgroundColor: 'pink',
+          transition: ' 2s all'
+        })
+      );
+    },
+    function () {
+      $(this).css(
+        {
+          transform: 'rotate(-360deg)',
+          backgroundColor: 'rgb(188, 216, 223)',
+          transition: '3s all'
+        },
+        $('div.elem__square').css({
+          backgroundColor: 'rgb(188, 216, 223)',
+          transition: ' 2s all'
+        })
+      );
+    }
+  );
 });
