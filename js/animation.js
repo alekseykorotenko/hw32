ec48610b4a9e1c4d4883da23286d4ea6411712fc
alekseykorotenko <alekor86@gmail.com>
2024-01-22 15:14:00 +0200

    add info about copyright, add jquery animation

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,15 @@
-$('li.nav__item').click(function () {
-  $(this).animate({
-    opacity: '0.8'
+$(function () {
+  $('li.nav__item').click(function () {
+    $(this).css({
+      opacity: '0.6',
+      transition: '2s all'
+    });
+  });
+
+  $('h1.header__heading').click(function () {
+    $(this).css({
+      color: 'black',
+      transition: '3s all'
+    });
   });
 });
